223cdee8fe5e9f7de85758c29d8fde5b2f023943
DesM0S <desaspiration@gmail.com>
2022-06-27 00:08:09 +0300

    fixed info modal

--- scripts/tour.js
+++ scripts/tour.js
@@ -1,69 +1,58 @@
 //слайды
-let slides = document.querySelectorAll('.tour-slide')
+let slides = document.querySelectorAll(".tour-slide");
 
 // определение на какой слайд нужно переключиться
-let tourButtonWhere = 0
+let tourButtonWhere = 0;
 
 // переход по слайдам -- вперед
-let tourButtonNext = document.querySelectorAll('.tour-btn')
-openResult(tourButtonNext)
+let tourButtonNext = document.querySelectorAll(".tour-btn");
+openResult(tourButtonNext);
 
 //переход по слайдам -- назад
-let tourButtonBack = document.querySelectorAll('.tour-btn--back')
-openResult(tourButtonBack)
+let tourButtonBack = document.querySelectorAll(".tour-btn--back");
+openResult(tourButtonBack);
 
-let tourInfo = document.querySelectorAll('.tour-btn-info')
-openResult(tourInfo)
+let tourInfo = document.querySelectorAll(".tour-btn-info");
+openResult(tourInfo);
+
+const handleClickContainerSlide = (targetModal) => {
+  if (targetModal) {
+    targetModal.classList.remove("tour-info-modal--active");
+  }
+};
+
+Array.from(document.querySelectorAll(".tour-info-modal")).forEach(modal => {
+  modal.addEventListener('click', (e) => {
+    e.stopPropagation();
+  })
+})
 
 //переход по слайдам
-function openResult(e) {
-	e.forEach(e => {
-		e.addEventListener('click', () => {
-			let a = e.dataset.tourInfoSlide
-			tourButtonWhere = e.dataset.toutWhere
+function openResult(el) {
+  el.forEach((e) => {
+    e.addEventListener("click", function (event) {
+      event.stopPropagation();
 
-			slides.forEach(e => {
-				if (e.dataset.tourSlide === tourButtonWhere) {
-					e.classList.add('tour-slide--active')
+      const element = e;
+      const parentElement = element.offsetParent;
+      let modal = parentElement.querySelector(".tour-info-modal");
+      console.log(element, parentElement, modal);
+      parentElement.addEventListener("click", () =>
+        handleClickContainerSlide(modal)
+      );
 
-				} else if (e.dataset.tourSlide === a) {
-					let element = e.querySelector('.tour-info-modal')
-					element.classList.toggle('tour-info-modal--active')
+      let a = element.dataset.tourInfoSlide;
+      tourButtonWhere = element.dataset.toutWhere;
 
-				} else {
-					e.classList.remove('tour-slide--active')
-				}
-			})
-		})
-	})
+      slides.forEach((e) => {
+        if (e.dataset.tourSlide === tourButtonWhere) {
+          e.classList.add("tour-slide--active");
+        } else if (e.dataset.tourSlide === a) {
+          modal.classList.toggle("tour-info-modal--active");
+        } else {
+          e.classList.remove("tour-slide--active");
+        }
+      });
+    });
+  });
 }
-
-
-// let ff = document.querySelectorAll('.tour-info-modal')
-// document.addEventListener('click', (e) => {
-	
-// 	if(!e.target.classList.contains('.tour-info-modal--active')){
-// 		console.log(!e.target.classList.contains('tour-info-modal--active'))
-		
-// 	} else{
-// 		console.log(e.target)
-// 		ff.forEach(e => {
-// 			e.classList.remove('tour-info-modal--active')
-// 		})
-// 	}
-// })
-
-
-// window.addEventListener('click', e => { // при клике в любом месте окна браузера
-// 	const target = e.target // находим элемент, на котором был клик
-// 	if (!target.closest('.tour-info-modal--active')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-// 		document.querySelector('.tour-info-modal--active').classList.remove('tour-info-modal--active') // то закрываем окно навигации, удаляя активный класс
-// 	}
-// })
-// document.addEventListener('click', function(e) {
-// 	console.log(e.target.class)
-//   if (e.target.class != document.querySelector('.tour-info-modal')) {
-// 		console.log('ss')
-//     // block.classList.add('hide');
-//   }
-// });
